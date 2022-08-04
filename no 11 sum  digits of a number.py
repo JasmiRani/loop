e3e4b2356ead68c931a6699bb82788fb534e6d55
JasmiRani <106468684+JasmiRani@users.noreply.github.com>
2022-08-04 09:05:21 +0530

    Update no 11 sum  digits of a number.py

--- no 11 sum  digits of a number.py
+++ no 11 sum  digits of a number.py
@@ -1,7 +1,8 @@
 #.Write a program to find the sum of the digits of a number accepted from the user.
 a=int(input("enter number: "))
+i=0
 sum=0
-while a>0:
-    sum=sum+a%10
-    a=a//10
+while i<len(a):
+    sum=sum+int(a[i])
+    i=i+1
 print("sum of num is",sum)
